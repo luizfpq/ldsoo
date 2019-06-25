1c7443a97093bdb0ce12217d773ec19b5612abd9
Quirino <quirino@teldrassil.ist.local>
2019-06-25 09:47:38 -0300

    simplifying git pull

--- pull.php
+++ pull.php
@@ -1,11 +1,7 @@
 <?php
 
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-
-if ( $_POST['payload'] ) {
-shell_exec( ‘cd /var/www/html/ldsoo && git reset –hard HEAD && git pull >> temp ’ );
+shell_exec( ‘cd /var/www/html/ldsoo && git pull >> temp ’ );
 readfile('temp');
 echo file_get)contents('temp');
-}
 
 ?>
