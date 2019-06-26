3ef03576ce34acda784fd9722390712106a95b17
Lucas Fidelis <fidelis.lucas@gmail.com>
2019-06-25 21:30:09 -0400

    Criação da tela de logon

--- view/logon/logon.php
+++ view/logon/logon.php
@@ -1,192 +1,38 @@
-<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="assets/img/logo-2.png">
 
-<!--
-Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
-<html lang="pt-BR">
+    <title>DOE-SE | Identifique-se</title>
 
-	<!-- begin::Head -->
-	<head>
-		<meta charset="utf-8" />
-		<title>DOE - SE &middot; Login</title>
-		<meta name="description" content="Latest updates and statistic charts">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    
+    <!-- Estilos customizados para esse template -->    
+    <link href="assets/style/signin.css" rel="style/stylesheet">
 
-		<!--begin::Web font -->
-		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-		<script>
-			WebFont.load({
-            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
-            active: function() {
-                sessionStorage.fonts = true;
-            }
-          });
-        </script>
+    <!-- Principal CSS do Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-		<!--end::Web font -->
+  </head>
 
-		<!--begin::Global Theme Styles -->
-		<link href="assets/vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
+  <body class="text-center container">
 
-		<!--RTL version:<link href="assets/vendors/base/vendors.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
-		<link href="assets/demo/base/style.bundle.css" rel="stylesheet" type="text/css" />
-
-		<!--RTL version:<link href="assets/demo/default/base/style.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
-
-		<!--end::Global Theme Styles -->
-		<link rel="shortcut icon" href="assets/img/logo-2.png" />
-		<link rel="icon" href="assets/img/logo-2.png" />
-	</head>
-
-	<!-- end::Head -->
-
-	<!-- begin::Body -->
-	<body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
-
-		<!-- begin:: Page -->
-		<div class="m-grid m-grid--hor m-grid--root m-page">
-			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-1" id="m_login" style="background-image: url(assets/img/bg.jpg);">
-				<div class="m-grid__item m-grid__item--fluid m-login__wrapper">
-					<div class="m-login__container">
-						<div class="m-login__logo">
-							<a href="#">
-								<img src="assets/img/logo-1.png">
-							</a>
-						</div>
-						<div class="m-login__signin">
-							<div class="m-login__head">
-								<h3 class="m-login__title"></h3>
-							</div>
-							<form class="m-login__form m-form" action="index.php?controller=Logon&action=logon" method="post">
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="email" placeholder="Email" name="email" id="inputEmail" autocomplete="off">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Senha" name="password" id="inputPassword" required>
-								</div>
-								<div class="row m-login__form-sub">
-									<div class="col m--align-left m-login__form-left">
-										<label class="m-checkbox  m-checkbox--light">
-											<input type="checkbox" name="remember"> Lembrar-me
-											<span></span>
-										</label>
-									</div>
-<!-- habilitar recuperação de senha
-									<div class="col m--align-right m-login__form-right">
-										<a href="javascript:;" id="m_login_forget_password" class="m-link">Esqueceu a senha?</a>
-									</div>
--->
-								</div>
-								<div class="m-login__form-action">
-									<button id="m_login_signin_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primary">Entrar</button>
-								</div>
-							</form>
-						</div>
-<!-- habilitar criar conta
-						<div class="m-login__signup">
-							<div class="m-login__head">
-								<h3 class="m-login__title">Entrar</h3>
-								<div class="m-login__desc">Entre com os dados para criar a conta:</div>
-							</div>
-							<form class="m-login__form m-form" action="index.php?controller=Logon&action=logon" method="post">
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="text" placeholder="Fullname" name="fullname">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="text" placeholder="Email" name="email" autocomplete="off">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="password" placeholder="Password" name="password">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Confirm Password" name="rpassword">
-								</div>
-								<div class="row form-group m-form__group m-login__form-sub">
-									<div class="col m--align-left">
-										<label class="m-checkbox m-checkbox--light">
-											<input type="checkbox" name="agree">Eu concordo com os <a href="#" class="m-link m-link--focus">termos and condi&ccedil;&otilde;es</a>.
-											<span></span>
-										</label>
-										<span class="m-form__help"></span>
-									</div>
-								</div>
-								<div class="m-login__form-action">
-									<button id="m_login_signup_submit" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">Cadastrar</button>&nbsp;&nbsp;
-									<button id="m_login_signup_cancel" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn">Cancelar</button>
-								</div>
-							</form>
-						</div>
--->
-<!-- habilitar recuperação de senha
-
-						<div class="m-login__forget-password">
-							<div class="m-login__head">
-								<h3 class="m-login__title">Esqueceu sua senha?</h3>
-								<div class="m-login__desc">Seu email para redefinir sua senha:</div>
-							</div>
-							<form class="m-login__form m-form" action="index.php?controller=Logon&action=logon" method="post" >
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" placeholder="Email" type="email" name="email" id="inputEmail" autocomplete="off">
-								</div>
-								<div class="m-login__form-action">
-									<button id="m_login_forget_password_submit" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">Requisitar</button>&nbsp;&nbsp;
-									<button id="m_login_forget_password_cancel" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn">Cancelar</button>
-								</div>
-							</form>
-						</div>
--->
-
-<!-- habilitar cadastro
-						<div class="m-login__account">
-							<span class="m-login__account-msg">
-								Ainda não está cadastrado?
-							</span>&nbsp;&nbsp;
-							<a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link">Cadastrar</a>
-						</div>
--->
-					</div>
-				</div>
-			</div>
-		</div>
-<!--  modelo prof
-		<form action="index.php?controller=Logon&action=logon" method="post" class="form-signin">
-		  <img class="mb-4" src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-		  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-		  <label for="inputEmail" class="sr-only">Email address</label>
-		  <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-		  <label for="inputPassword" class="sr-only">Password</label>
-		  <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-		  <div class="checkbox mb-3">
-		    <label>
-		      <input type="checkbox" value="remember-me"> Remember me
-		    </label>
-		  </div>
-		  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-		  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
-		</form>
--->
-		<!-- end:: Page -->
-
-		<!--begin::Global Theme Bundle -->
-		<script src="assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
-		<script src="assets/demo/base/scripts.bundle.js" type="text/javascript"></script>
-
-		<!--end::Global Theme Bundle -->
-
-		<!--begin::Page Scripts
-		<script src="assets/snippets/custom/pages/user/login6.js" type="text/javascript"></script>
--->
-		<!--end::Page Scripts -->
-	</body>
-
-	<!-- end::Body -->
+  	<div class="row">
+  		<form class="form-signin col-10 offset-1">
+	      <img class="mb-4" src="assets/img/logo-2.png" alt="" width="72" height="72">
+	      <h1 class="h3 mb-3 font-weight-normal">Faça login</h1>
+	      <label for="inputEmail" class="sr-only">Endereço de email</label>
+	      <input type="email" id="inputEmail" class="form-control" placeholder="Seu email" required autofocus>
+	      <label for="inputPassword" class="sr-only">Senha</label>
+	      <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+	      <div class="checkbox mb-3">
+	        <label>
+	          <input type="checkbox" value="remember-me"> Lembrar de mim
+	        </label>
+	      </div>
+	      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+	      <p class="mt-5 mb-3 text-muted">DOE-SE &copy; 2019</p>
+	    </form>
+  	</div>
+  </body>
 </html>
