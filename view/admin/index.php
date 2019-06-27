dae55559ab1e8dc7524bf573e1e4aaf0e3f0c65d
Lucas Fidelis <fidelis.lucas@gmail.com>
2019-06-26 22:04:47 -0400

    página inicial da dashboard

--- view/admin/index.php
+++ view/admin/index.php
@@ -4,20 +4,93 @@
 <?php include 'partials/header.php'?>
 
 <div class="container-fluid">
-	<div class="row h-100">
-		<div class="col-md-2 sidebar bg-light">
-			<?php include 'partials/sidebar.php' ?>
-		</div>	
+	<div class="row">
+		<!--div class="col-md-2 sidebar bg-light">
+			<?php //include 'partials/sidebar.php' ?>
+		</div-->	
 <!-- Conteudo -->
-		<div class="col">
+		<div class="content col-10 offset-md-1">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col">
-						<div id='calendar-wrapp'>
-							<div id='calendar'></div>
-							<div style='clear:both'></div>
-						</div>
+						<h1><i class="fas fa-home"></i> Início</h1>
+						<hr>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porta nulla ut pharetra aliquam. Vivamus auctor vitae metus sed suscipit. Mauris lacinia, velit in iaculis finibus, lacus mi tempor leo, vitae hendrerit dolor ligula a urna.
+						</p>
+						<button class="btn btn-secondary"><i class="fas fa-plus"></i> Saiba mais</button>
 					</div>
+					<div class="card col">
+						<div class="card-header">
+							<h5 class="card-title float-left"><i class="fas fa-calendar-check"></i> Próximos eventos</h5>
+							<button class="btn btn-sm btn-secondary float-right"><i class="fas fa-plus"></i> Mais eventos</button>
+						</div>					  
+					  <div class="card-body">
+					    <div class="list-group">
+						  <a href="#" class="list-group-item list-group-item-action">
+						    <div class="d-flex w-100 justify-content-between">
+						      <h5 class="mb-1">Leilão beneficente</h5>
+						      <small>Em 3 dias</small>
+						    </div>
+						    <p class="mb-1">Donec id elit non mi porta gravida at eget metus.</p>
+						  </a>
+						  <a href="#" class="list-group-item list-group-item-action">
+						    <div class="d-flex w-100 justify-content-between">
+						      <h5 class="mb-1">Leilão beneficente</h5>
+						      <small>Em 3 dias</small>
+						    </div>
+						    <p class="mb-1">Donec id elit non mi porta gravida at eget metus.</p>
+						  </a>
+						  <a href="#" class="list-group-item list-group-item-action">
+						    <div class="d-flex w-100 justify-content-between">
+						      <h5 class="mb-1">Leilão beneficente</h5>
+						      <small>Em 3 dias</small>
+						    </div>
+						    <p class="mb-1">Donec id elit non mi porta gravida at eget metus.</p>
+						  </a>
+						</div>
+					  </div>
+					</div>
+				</div>
+				
+				<div class="row">
+
+					<div class="card activities resume col">
+						<a href="#">
+							<img src="assets\img\logon-bg.jpg" class="card-img-top" alt="...">
+						  	<div class="card-body">					  	
+					  		<h6><i class="fas fa-tasks"></i> Atividades</h6>
+							<p>
+								Cadastre, atualize, remova e atribua novas atividades a ONG e aos voluntários.
+							</p>
+					  	</div>
+					  	</a>
+					</div>
+
+					<div class="card sectors resume col">
+						<a href="#">
+							<img src="assets\img\logon-bg.jpg" class="card-img-top" alt="...">
+						  	<div class="card-body">					  	
+					  		<h6><i class="fas fa-chart-pie"></i> Setores</h6>
+							<p>
+								Cadastre, atualize, remova e atribua novas atividades a ONG e aos voluntários.
+							</p>
+					  	</div>
+					  	</a>
+					</div>
+
+					<div class="card volunteers resume col">
+						<a href="#">
+							<img src="assets\img\logon-bg.jpg" class="card-img-top" alt="...">
+						  	<div class="card-body">					  	
+					  		<h6><i class="fas fa-hands-helping"></i> Voluntários</h6>
+							<p>
+								Cadastre, atualize, remova e atribua novas atividades a ONG e aos voluntários.
+							</p>
+					  	</div>
+					  	</a>
+					</div>
+					
 				</div>
 			</div>
 		</div>
@@ -25,3 +98,4 @@
 </div>
 <!-- Fim do conteúdo -->
 <?php include 'partials/footer.php'?>
+</body>
