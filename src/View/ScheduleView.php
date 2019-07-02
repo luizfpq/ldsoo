936096df12307e938180af00d7584449123de273
Lucas Fidelis <fidelis.lucas@gmail.com>
2019-07-01 21:40:27 -0400

    View da criação de Schedule

--- src/View/ScheduleView.php
+++ src/View/ScheduleView.php
@@ -5,12 +5,40 @@ class ScheduleView
 
 	const scheduleRoute = 'view/schedule/index.php';
 
+	const createRoute = 'view/schedule/create.php';
+
+  	const updateRoute = 'view/schedule/update.php';
+
+  	const deleteRoute = 'view/schedule/delete.php';
+
+  	const listRoute = 'view/schedule/list.php';
+
   	public function __construct(){}
 
   	public function getScheduleRoute(){
 
     	return self::scheduleRoute;
   	}
+
+  	public function getCreateRoute(){
+
+	    return self::createRoute;
+	  }
+
+	  public function getUpdateRoute(){
+
+	    return self::updateRoute;
+	  }
+
+	  public function getDeleteRoute(){
+
+	    return self::deleteRoute;
+	  }
+
+	  public function getListRoute(){
+
+	    return self::listRoute;
+	  }
 
 
 }
--- view/default/head.php
+++ view/default/head.php
@@ -5,6 +5,8 @@
   	<link href="https://fonts.googleapis.com/css?family=Poppins:100,400,800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/air-datepicker/2.2.3/css/datepicker.css">
+
   	
 
 	<title><?php echo $page_title;?> | DOE-SE</title>
@@ -20,4 +22,6 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/e07306bc16.js"></script>
   <script src="https://cdn.jsdelivr.net/combine/npm/@fullcalendar/core@4.2.0,npm/@fullcalendar/core@4.2.0/locales/pt-br.min.js,npm/@fullcalendar/daygrid@4.2.0"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/air-datepicker/2.2.3/js/datepicker.js" type="text/javascript"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/air-datepicker/2.2.3/js/i18n/datepicker.pt-BR.js" type="text/javascript"></script>
 </head>
