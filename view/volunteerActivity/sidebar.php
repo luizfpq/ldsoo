c09ffcb2807142e4cb077309715d556659e939e5
LuizQuirino <luizfpq@gmail.com>
2019-07-04 18:03:24 -0300

    fix sidebar

--- view/volunteerActivity/sidebar.php
+++ view/volunteerActivity/sidebar.php
@@ -1,9 +1,10 @@
 <!-- Sidebar -->
     <div id="sidebar-wrapper">
       <div class="list-group list-group-flush">
-        <h6 class="list-group-item"><i class="fas fa-cogs"></i> Ações</h6>
+      <!--  <h6 class="list-group-item"><i class="fas fa-cogs"></i> Ações</h6>
         <a href="?controller=Volunteer&action=create" class="list-group-item list-group-item-action"><i class="fas fa-plus"></i> Novo</a>
         <a href="?controller=Volunteer&action=Volunteer" class="list-group-item list-group-item-action"><i class="fas fa-clipboard-list"></i> Todos voluntários</a>
+      -->
       </div>
     </div>
 <!-- /#sidebar-wrapper -->
